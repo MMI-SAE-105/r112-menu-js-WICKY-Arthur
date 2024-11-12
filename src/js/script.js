247690a46123fc613d44fc754b1017c1e8865824
WICKY-Arthur <arthur.wicky@edu.univ-fcomte.fr>
2024-11-12 17:25:40 +0100

    classe 'noscroll'

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,20 @@
 // On cible les éléments à modifier
 const toggle = document.querySelector("button");
 const nav = document.querySelector("nav");
+const body = document.querySelector("body");
 
 // Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
 toggle.addEventListener("click", () => {
     const isOpen = toggle.ariaExpanded === "true";
     const isClosed = !isOpen;
-    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
     // Mise à jour des attributs ARIA pour accessibilité
     nav.ariaHidden = false;
     toggle.ariaExpanded = true;
+    body.classList.add("noscroll");
     if (isOpen == true) {
         nav.ariaHidden = true;
         toggle.ariaExpanded = false;
+        body.classList.remove("noscroll");
     }
 });
+
