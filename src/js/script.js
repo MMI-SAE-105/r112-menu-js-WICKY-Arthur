06a72bb6c1aa41509f6739384876867c80d5714a
WICKY-Arthur <arthur.wicky@edu.univ-fcomte.fr>
2024-11-12 17:18:25 +0100

    Ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,14 @@ const nav = document.querySelector("nav");
 
 // Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
 toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
     // Mise à jour des attributs ARIA pour accessibilité
     nav.ariaHidden = false;
     toggle.ariaExpanded = true;
+    if (isOpen == true) {
+        nav.ariaHidden = true;
+        toggle.ariaExpanded = false;
+    }
 });
